7f596e736a9b74f69fea1d6b0307d01d55bd1d99
leodeymison <leodeymisonbasquete@gmail.com>
2022-07-08 15:40:20 -0300

    small product mobile

--- src/components/layout/ProductHome/style.ts
+++ src/components/layout/ProductHome/style.ts
@@ -3,12 +3,16 @@ import styled from "styled-components";
 export const _Card = styled('div')`
     display: flex;
     flex-direction: column;
+    align-items: center;
     border: solid 1px #aaa;
     width: 45%;
     padding: 1em;
     margin: 0.5%;
     @media (max-width: 888px){
         width: 100%;
+        img {
+            width: 60%;
+        }
     }
 `
 export const _Img = styled('img')`
